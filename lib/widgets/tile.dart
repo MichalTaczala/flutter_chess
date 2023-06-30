dfab23cdc737f5e47f7d076690443e1f8da20172
MichalTaczala <michtaczala@gmail.com>
2023-06-30 13:11:48 +0200

    add figures' images

--- lib/widgets/tile.dart
+++ lib/widgets/tile.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chess/enums/figure_type_enum.dart';
 import 'package:flutter_chess/models/figure.dart';
+import 'package:flutter_svg_provider/flutter_svg_provider.dart';
 
 class Tile extends StatelessWidget {
   final bool isBlack;
@@ -18,10 +20,37 @@ class Tile extends StatelessWidget {
     this.isUnderAttack = false,
   });
 
+  String getPathForFigure(Figure fig) {
+    String basicPath = "assets/images/";
+    basicPath += fig.isWhite ? "svg_white/" : "svg_black/";
+    final type = fig.figureType;
+    switch (type) {
+      case FigureTypeEnum.bishop:
+        return "${basicPath}chess-bishop-icon.svg";
+      case FigureTypeEnum.king:
+        return "${basicPath}chess-king-icon.svg";
+      case FigureTypeEnum.queen:
+        return "${basicPath}chess-queen-icon.svg";
+      case FigureTypeEnum.rook:
+        return "${basicPath}chess-rook-icon.svg";
+      case FigureTypeEnum.pawn:
+        return "${basicPath}chess-pawn-icon.svg";
+      case FigureTypeEnum.knight:
+        return "${basicPath}chess-knight-icon.svg";
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
+        image: figure != null
+            ? DecorationImage(
+                image: Svg(
+                  getPathForFigure(figure!),
+                ),
+              )
+            : null,
         color: isCurrentlyClicked
             ? Colors.amber
             : isUnderAttack
@@ -32,9 +61,6 @@ class Tile extends StatelessWidget {
                         ? const Color.fromARGB(255, 82, 82, 82)
                         : const Color.fromARGB(255, 227, 227, 227),
       ),
-      // color: Colors.red),
-      child: Text(
-          figure != null ? figure!.figureType.toString() : index.toString()),
     );
   }
 }
